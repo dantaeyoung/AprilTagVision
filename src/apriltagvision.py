b7c1f6e37a1d3ac4b0dbd17ceb3a6267d2fe1d2d
Dan Taeyoung <dan@dantaeyoung.com>
2024-03-13 20:22:32 -0400

    tidying up apriltagvision code

--- src/apriltagvision.py
+++ src/apriltagvision.py
@@ -18,10 +18,10 @@ osc_client = SimpleUDPClient(ip, port)
 # Variable to track pause state
 paused = False
 
+# Track visible tags and their last known positions
+past_tags = {}
 
-def send_tuio_messages(messages):
-    for m in messages:
-        osc_client.send_message(m[0], m[1])
+
 
 def handle_key_press(key):
     global paused
@@ -43,12 +43,70 @@ def detect_apriltags(gray):
 
 
 
-def create_displayframe(gray, tags, current_tags):
+def generate_tuio_messages(rawtags, frame_width, frame_height):
+    global verbose
+    # send data via OSC using the TUIO protocol
+
+    # the reason why I'm incrementing 100 is because these are temporary session ids, not fiducial marker ids. It's helpful to increment at an arbitrary number so that the arbitrary-ness isclear
+    arbitrary_sessionid_start = 100
+
+    # we gather all the messages before sending at once
+    tuio_messages = []
+
+    tuio_messages.append(["/tuio/2Dobj", ["source", "apriltagvision"]])
+    tuio_messages.append(["/tuio/2Dobj", ["alive"] + list(range(arbitrary_sessionid_start, arbitrary_sessionid_start+len(rawtags)))])
+
+
+    current_tags = {}
+
+    ## the same arbitrary number
+    session_id = arbitrary_sessionid_start 
+    for tag in rawtags:
+        session_id += 1
+
+        current_tag = {}
+
+        # x and y position
+        current_tag['x'] = utils.round_and_threshold(tag.center[0] / frame_width, 0.0005, 7)
+        current_tag['y'] = utils.round_and_threshold(tag.center[1] / frame_height, 0.0005, 7)
+
+        # angle in radians
+        current_tag['a'] = utils.round_and_threshold(utils.extract_rotation(tag.homography), 0.05, 4)
+
+        # x and y and angle velocity (calculated as the delta from last tag)
+        current_tag['X'] = 0
+        current_tag['Y'] = 0
+        current_tag['A'] = 0
+        if tag.tag_id in past_tags:
+            ptag = past_tags[tag.tag_id]
+            tuio_X = utils.round_and_threshold((tag.center[0] / frame_width) - ptag['x'], 0.0005, 7)
+            tuio_Y = utils.round_and_threshold((tag.center[1] / frame_height) - ptag['y'], 0.0005, 7)
+            current_tag['X'] = tuio_X
+            current_tag['Y'] = tuio_Y
+            current_tag['A'] = utils.round_and_threshold(utils.angular_difference(ptag['a'], current_tag['a']), 0.05, 4)
+
+        # motion and rotation acceleration.. not calculated for now
+        current_tag['m'] = 0
+        current_tag['r'] = 0
+
+        tuio_messages.append(["/tuio/2Dobj", ["set", session_id, tag.tag_id, current_tag['x'], current_tag['y'], current_tag['a'], current_tag['X'], current_tag['Y'], current_tag['A'], current_tag['m'], current_tag['r']]])
+
+        current_tags[tag.tag_id] = current_tag
+
+        if(verbose):
+            print(tag)
+
+    return tuio_messages, current_tags
+
+
+
+
+def create_displayframe(gray, rawtags, current_tags):
     # Create a copy of the grayscale frame to draw colored polylines
     displayframe = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
 
     # Visualize detected tags
-    for tag in tags:
+    for tag in rawtags:
         cv2.polylines(displayframe, [tag.corners.astype(int)], isClosed=True, color=(0, 255, 0), thickness=2)
 
         rotated_text_img, _, _ = utils.putRotatedText(displayframe, str(tag.tag_id), (int(tag.center[0]), int(tag.center[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, current_tags[tag.tag_id]['a'])
@@ -57,8 +115,47 @@ def create_displayframe(gray, tags, current_tags):
 
 
 
+def send_messages(tuio_messages, past_tags, current_tags):
+    global last_negfseq_time
+    global tuio_fseq
+    global verbose
+   
+    # the last thing we send is a 'fseq' with the fseq id.
+    # however, if the last two frames were exactly the same, send '-1'.
+    # technically we should compare the last two frames. but in practice, this only happens when the last frame, as well as this frame, was completely free of tags.
+
+
+
+    if(len(past_tags) == 0 and len(current_tags) == 0):
+        # we also check the time to mimic reactivision behavior, which is to only send consecutive '-1' fseq messages once a second after the first one
+        current_time = time.time()
+        if(current_time - last_negfseq_time > NEG_FSEQ_FREQ):
+            tuio_messages.append(["/tuio/2Dobj", ["fseq", -1]])
+            for m in tuio_messages:
+                osc_client.send_message(m[0], m[1])
+            if(verbose):
+                print(tuio_messages)
+            last_negfseq_time = current_time
+    else:
+        tuio_messages.append(["/tuio/2Dobj", ["fseq", tuio_fseq]])
+        for m in tuio_messages:
+            osc_client.send_message(m[0], m[1])
+        if(verbose):
+            print(tuio_messages)
+        last_negfseq_time = 0
+
+    # increment fseq
+    tuio_fseq += 1
+
+
+
 def main():
     global paused
+    global past_tags
+    global last_negfseq_time
+    global tuio_fseq
+    global verbose
+
     args = utils.parse_arguments()
     camera_index = args.camera
     frame_width = args.width
@@ -69,8 +166,6 @@ def main():
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
-    # Track visible tags and their last known positions
-    past_tags = {}
 
     tuio_fseq = 0
     last_negfseq_time = 0
@@ -94,96 +189,22 @@ def main():
         # make frame grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-
         # detect apriltags
-        tags = detect_apriltags(gray)
+        rawtags = detect_apriltags(gray)
+
+        tuio_messages, current_tags = generate_tuio_messages(rawtags, frame_width, frame_height)
 
 
+        send_messages(tuio_messages, past_tags, current_tags)
 
-        # send data via OSC using the TUIO protocol
-
-        # the reason why I'm incrementing 100 is because these are temporary session ids, not fiducial marker ids. It's helpful to increment at an arbitrary number so that the arbitrary-ness isclear
-        arbitrary_sessionid_start = 100
-
-        # we gather all the messages before sending at once
-        tuio_messages = []
-
-        tuio_messages.append(["/tuio/2Dobj", ["source", "apriltagvision"]])
-        tuio_messages.append(["/tuio/2Dobj", ["alive"] + list(range(arbitrary_sessionid_start, arbitrary_sessionid_start+len(tags)))])
-
-    
-        current_tags = {}
-
-        ## the same arbitrary number
-        session_id = arbitrary_sessionid_start 
-        for tag in tags:
-            session_id += 1
-
-            current_tag = {}
-
-            # x and y position
-            current_tag['x'] = utils.round_and_threshold(tag.center[0] / frame_width, 0.0005, 7)
-            current_tag['y'] = utils.round_and_threshold(tag.center[1] / frame_height, 0.0005, 7)
-
-            # angle in radians
-            current_tag['a'] = utils.round_and_threshold(utils.extract_rotation(tag.homography), 0.05, 4)
-
-            # x and y and angle velocity (calculated as the delta from last tag)
-            current_tag['X'] = 0
-            current_tag['Y'] = 0
-            current_tag['A'] = 0
-            if tag.tag_id in past_tags:
-                ptag = past_tags[tag.tag_id]
-                tuio_X = utils.round_and_threshold((tag.center[0] / frame_width) - ptag['x'], 0.0005, 7)
-                tuio_Y = utils.round_and_threshold((tag.center[1] / frame_height) - ptag['y'], 0.0005, 7)
-                current_tag['X'] = tuio_X
-                current_tag['Y'] = tuio_Y
-                current_tag['A'] = utils.round_and_threshold(utils.angular_difference(ptag['a'], current_tag['a']), 0.05, 4)
-
-            # motion and rotation acceleration.. not calculated for now
-            current_tag['m'] = 0
-            current_tag['r'] = 0
-
-            tuio_messages.append(["/tuio/2Dobj", ["set", session_id, tag.tag_id, current_tag['x'], current_tag['y'], current_tag['a'], current_tag['X'], current_tag['Y'], current_tag['A'], current_tag['m'], current_tag['r']]])
-
-            current_tags[tag.tag_id] = current_tag
-
-            if(verbose):
-                print(tag)
-
-   
-
-        # the last thing we send is a 'fseq' with the fseq id.
-        # however, if the last two frames were exactly the same, send '-1'.
-        # technically we should compare the last two frames. but in practice, this only happens when the last frame, as well as this frame, was completely free of tags.
-
-        if(len(past_tags) == 0 and len(current_tags) == 0):
-            # we also check the time to mimic reactivision behavior, which is to only send consecutive '-1' fseq messages once a second after the first one
-            current_time = time.time()
-            if(current_time - last_negfseq_time > NEG_FSEQ_FREQ):
-                tuio_messages.append(["/tuio/2Dobj", ["fseq", -1]])
-                send_tuio_messages(tuio_messages)
-                if(verbose):
-                    print(tuio_messages)
-                last_negfseq_time = current_time
-        else:
-            tuio_messages.append(["/tuio/2Dobj", ["fseq", tuio_fseq]])
-            send_tuio_messages(tuio_messages)
-            if(verbose):
-                print(tuio_messages)
-            last_negfseq_time = 0
 
         # store our current tags as the past tags, so the next time around, we can calculate
         past_tags = current_tags
 
-        # increment fseq
-        tuio_fseq += 1
-
-
                
 
         # Display the frame
-        displayframe = create_displayframe(gray, tags, current_tags)
+        displayframe = create_displayframe(gray, rawtags, current_tags)
         cv2.imshow('AprilTagVision (press p to pause, q to quit)', displayframe)
 
 
